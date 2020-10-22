78acf04bebf2bd35b42cc66959b94ebf67ee9ffa
KevinChevez <kchevez@espol.edu.ec>
2020-10-21 19:51:28 -0500

    espacio

--- Snake/src/lab1/Main.java
+++ Snake/src/lab1/Main.java
@@ -22,6 +22,8 @@ public class Main {
 		// pack() will do the layout of the window so it gets the correct size
 		frame.pack();
 
+                
+                
 		// Open the window
 		frame.setVisible(true);
 		frame.requestFocus();
